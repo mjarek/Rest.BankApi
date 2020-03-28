3aeb0c6289de65965a6a0adfede5fd7c307306f0
mjarek <mjarek1313@gmail.com>
2020-03-28 21:01:29 +0100

    Added needed operations

--- Rest.BankApi.Services/Account .cs
+++ Rest.BankApi.Services/Account .cs
@@ -14,5 +14,32 @@ namespace Rest.BankApi.Services
         public decimal Balance { get; set; }
         private bool _isOpen = true;
         private bool _isFreeze;
+
+        public void Withdrawal(decimal amount)
+        {
+            Balance -= amount;
+        }
+
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+
+        private bool IsVerified()
+        {
+            return Status == StatusAccount.Verified;
+        }
+
+        private bool IsOpen()
+        {
+            return _isOpen;
+        }
+
+        private bool IsFreeze()
+        {
+            return _isFreeze;
+        }
+
+
     }
 }
