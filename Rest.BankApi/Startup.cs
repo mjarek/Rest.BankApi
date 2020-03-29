9f8072b3bfb3578f41def7a8d659487f8df255e2
mjarek <mjarek1313@gmail.com>
2020-03-29 16:31:29 +0200

    added swager

--- Rest.BankApi/Startup.cs
+++ Rest.BankApi/Startup.cs
@@ -3,8 +3,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
-namespace Rest.BankApi
+  namespace Rest.BankApi
 {
     public class Startup
     {
@@ -19,11 +20,18 @@ namespace Rest.BankApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen((options) =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo {Version = "v1", Title = "Bank Api"});
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(end => { end.SwaggerEndpoint("/swagger/v1/swagger.json", "Bank Api v1"); });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
