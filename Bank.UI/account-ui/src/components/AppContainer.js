46f1fafe4d81b97e65a84db387d054647f0521a0
mjarek <mjarek1313@gmail.com>
2020-03-30 09:22:50 +0200

    refactored DepositButton

--- Bank.UI/account-ui/src/components/AppContainer.js
+++ Bank.UI/account-ui/src/components/AppContainer.js
@@ -9,25 +9,39 @@ class AppContainer extends React.Component {
     this.state={balance: '',
                 status: '' }
     this.callWithdrawal=this.callWithdrawal.bind(this)
+    this.callDeposit=this.callDeposit.bind(this)
     }
     callWithdrawal() {
         fetch('http://localhost:52233/api/Accounts/b8bfc789-ad06-4820-9f4a-906e90aa8d49/Withdrawal', {
-          method: 'PUT',
-          body: JSON.stringify({
-            "amount": 250
-          }),
-          headers: {
-            "Content-type": "application/json; charset=UTF-8"
-          }
+            method: 'PUT',
+            body: JSON.stringify({
+                "amount": 250
+            }),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
         })
-      .then(response =>response.json())
-      .then(response => {
-      
-        console.log(response)
-        this.setState({balance: response.balance, status: response.statusProduct})
- 
-      })
-        }
+            .then(response => response.json())
+            .then(response => {
+                this.setState({ balance: response.balance, status: response.statusProduct })
+            })
+    }
+
+    callDeposit() {
+        fetch('http://localhost:52233/api/Accounts/b8bfc789-ad06-4820-9f4a-906e90aa8d49/Deposit', {
+            method: 'PUT',
+            body: JSON.stringify({
+                "amount": 1000
+            }),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        })
+            .then(response => response.json())
+            .then(response => {
+                this.setState({ balance: response.balance, status: response.statusProduct })
+            })
+    }
     render(){
         console.log(this.state)
       return (
@@ -37,7 +51,7 @@ class AppContainer extends React.Component {
             transform: 'translate(-50%, -50%)'
         }}
       >
-          <DepositButton />
+          <DepositButton onClickHandler={this.callDeposit}/>
           <WithdrawalButton onClickHandler={this.callWithdrawal}/>
           <ResultTable balance ={this.state.balance} status={this.state.status} />
       </div>
--- Bank.UI/account-ui/src/components/DepositButton.js
+++ Bank.UI/account-ui/src/components/DepositButton.js
@@ -2,26 +2,11 @@ import React from 'react';
 import styles from '../css/button.css'
 
 class DepositButton extends React.Component {
-    callDeposit() {
-      fetch('http://localhost:52233/api/Accounts/b8bfc789-ad06-4820-9f4a-906e90aa8d49/Deposit', {
-        method: 'PUT',
-        body: JSON.stringify({
-          "amount": 1000
-        }),
-        headers: {
-          "Content-type": "application/json; charset=UTF-8"
-        }
-      })
-    .then(response =>response.json())
-    .then(response => {
-    
-      console.log(response)
-    })
-      }
+
       render(){
         return (
           <div>
-            <button className="btn green" onClick={this.callDeposit}>Deposit 1000 zł</button>
+            <button className="btn green" onClick={this.props.onClickHandler}>Deposit 1000 zł</button>
           </div>
 
         )
